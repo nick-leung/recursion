e3f438eb83ab083898c7c88d1c9cb09f384636c5
Nick Leung <nickleung+bitbucket@gmail.com>
2018-01-08 14:44:44 -0800

    All tests passed.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,42 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var str = '';
+  if (typeof(obj) === 'number' || typeof(obj) === 'boolean' || obj === null){
+    return str + obj;
+  } else if (typeof(obj) === 'string'){
+    return addQuotationMarks(obj);
+  } else if (typeof(obj) === 'object') {
+    if (Array.isArray(obj)) {
+      str += '[';
+      if (obj.length > 0) {
+        for (var i = 0; i < obj.length; i++) {
+          str += (i === 0) ? '' : ',';
+          str += stringifyJSON(obj[i]);
+        }
+      } 
+      return str + ']';
+    } else {
+      str += '{';
+      var entries = Object.entries(obj);
+      if (entries.length > 0) {
+        for (var i = 0; i < entries.length; i++) {
+          var key = entries[i][0];
+          var value = entries[i][1];
+          if (key === 'undefined' || key === 'functions') {
+            continue;
+          }
+          str += (i === 0) ? '' : ',';
+          str += addQuotationMarks(key) + ':' + stringifyJSON(value);
+        }
+      } 
+      return str + '}';
+    }
+  } else if (obj === undefined) {
+    return obj;
+  }
 };
+
+var addQuotationMarks = function(str) {
+  return '"' + str + '"';
+}
